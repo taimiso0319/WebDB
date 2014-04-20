7a55d576bfe037615b39de31aa1a884068f9e3f3
Keisuke Yasuda <keisuke0319@gmail.com>
2014-04-21 05:46:23 +0900

    git test

---
+++ js/accordion.js
@@ -0,0 +1,15 @@
+$(function(){
+     
+$(".accordion p").click(function(){
+    $(this).next("ul").slideToggle();
+    $(this).children("span").toggleClass("open");
+}); 
+ 
+$(".accordion dt").click(function(){
+    $(this).next("dd").slideToggle();
+    $(this).next("dd").siblings("dd").slideUp();
+    $(this).toggleClass("open");    
+    $(this).siblings("dt").removeClass("open");
+});
+ 
+});
